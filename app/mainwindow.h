65366cb15d0b97f1caf8c3ab661cea7621eedcd8
dips42 <dips42@mail.ru>
2020-01-23 09:25:34 +0700

    Using resizeEvent instead layout.

--- app/mainwindow.h
+++ app/mainwindow.h
@@ -19,23 +19,36 @@ public:
           nanogui::Screen(size, caption, resizable, fullscreen, colorBits, alphaBits, depthBits, stencilBits, nSamples, glMajor, glMinor)
     {
         using namespace nanogui;
+
+        mWindow = new Window(this, "Some window");
+
+        mTabs = new TabWidget(mWindow);
+        Widget* imagetab = mTabs->createTab("Captured Images");
+        //imagetab->setSize(Vector2i(800, 600));
+
         mColumns.resize(3);
         mRows.resize(3);
-        mLayout = new AdvancedGridLayout(mColumns, mRows, 20);
-        setLayout(mLayout);
+        mLayout = new AdvancedGridLayout(mColumns, mRows, 0);
+        mLayout->setColStretch(0, 0.33f);
+        mLayout->setRowStretch(0, 0.33f);
         mLayout->setColStretch(1, 0.33f);
         mLayout->setRowStretch(1, 0.33f);
 
-        mAplyButton = new Button(this, "Aply");
-        mLayout->setAnchor(mAplyButton, AdvancedGridLayout::Anchor(1, 1, 1, 1, Alignment::Middle, Alignment::Middle));
-        mAplyButton->setVisible(true);
+        imagetab->setLayout(mLayout);
+
+        mAplyButton = new Button(imagetab, "Aply");
+        mLayout->setAnchor(mAplyButton, AdvancedGridLayout::Anchor(2, 2, 1, 1, Alignment::Middle, Alignment::Middle));
+
+        mImageView = new ImageView(imagetab, 0);
+        //mImageView->setSize(Vector2i(800, 600));
+        mLayout->setAnchor(mImageView, AdvancedGridLayout::Anchor(0, 0, 1, 1, Alignment::Middle, Alignment::Middle));
 
         setBackground(nanogui::Color(127, 127, 127, 0.5));
-        setResizeCallback([&](nanogui::Vector2i)
+      /*  setResizeCallback([&](nanogui::Vector2i)
         {
             performLayout();
             drawAll();
-        });
+        });*/
 
         performLayout();
     }
@@ -55,6 +68,16 @@ public:
         return false;
     }
 
+    bool resizeEvent(const Eigen::Vector2i & size) override
+    {
+        mWindow->setPosition(Eigen::Vector2i(0,0));
+        mWindow->setSize(Eigen::Vector2i(width(), height()));
+
+        mTabs->setPosition(Eigen::Vector2i(0,0));
+        mTabs->setSize(Eigen::Vector2i(mWindow->width(), mWindow->height()));
+
+        return true;
+    }
 //    void draw(NVGcontext* ctx)
 //    {
 //        Screen::draw(ctx);
@@ -62,10 +85,17 @@ public:
 
 private:
 
+
     std::vector<int> mColumns;
     std::vector<int> mRows;
     nanogui::AdvancedGridLayout* mLayout;
+
+    nanogui::Window* mWindow;
+    nanogui::TabWidget* mTabs;
     nanogui::Button* mAplyButton;
+    nanogui::ImageView* mImageView;
+    GLuint mBackImage;
+
 };
 
 }//namespace fm
